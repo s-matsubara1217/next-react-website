82f158119e9a30ed6f0faf42fc42569a96c784db
s-matsubara <shohs12171217@gmail.com>
2023-12-04 14:19:06 +0900

    231204

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,38 @@
+import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 import PostBody from 'components/post-body'
 import Contact from 'components/contact'
 import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from "components/two-column";
+import Image from 'next/image'
+import eyecatch from 'images/about.jpg'
 
 export default function About() {
   return (
     <Container>
+      <Meta
+        pageTitle="アバウト"
+        pageDesc="About development activities"
+        pageImg={eyecatch.src}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.height}
+      />
 
       <Hero
         title="About"
         subtitle="About development activities"
       />
+
+      <figure>
+        <Image
+          src={eyecatch}
+          alt=""
+          layout="responsive"
+          sizes="(min-width: 1152px) 1152px, 100vw"
+          priority
+          placeholder="blur"
+        />
+      </figure>
 
       <TwoColumn>
         <TwoColumnMain>

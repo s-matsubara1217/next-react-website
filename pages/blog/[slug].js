1cebfb4b50f0b7474a29de286fefee35ae0d064e
s-matsubara <shohs12171217@gmail.com>
2023-12-05 13:10:35 +0900

    231205

---
+++ pages/blog/[slug].js
@@ -0,0 +1,86 @@
+import { getPostBySlug, getAllSlugs } from 'lib/api'
+import { extractText } from 'lib/extract-text'
+import Meta from 'components/meta'
+import Container from 'components/container'
+import PostHeader from 'components/post-header'
+import PostBody from 'components/post-body'
+import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from "components/two-column";
+import ConvertBody from 'components/convert-body'
+import PostCategories from 'components/post-categories'
+import Image from 'next/image'
+import { eyecatchLocal } from "lib/constants";
+
+export default function Post({
+  title,
+  publish,
+  content,
+  eyecatch,
+  categories,
+  description,
+}) {
+  return (
+    <Container>
+      <Meta
+        pageTitle={title}
+        pageDesc={description}
+        pageImg={eyecatch.url}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.height}
+      />
+
+      <article>
+        <PostHeader title={title} subtitle="Blog Article" publish={publish} />
+
+        <figure>
+          <Image
+            src={eyecatch.url}
+            alt=""
+            layout="responsive"
+            width={eyecatch.width}
+            height={eyecatch.height}
+            sizes="(min-width: 1152px) 1152px, 100vw"
+            priority
+          />
+        </figure>
+
+        <TwoColumn>
+          <TwoColumnMain>
+            <PostBody>
+              <ConvertBody contentHTML={content} />
+            </PostBody>
+          </TwoColumnMain>
+          <TwoColumnSidebar>
+            <PostCategories categories={categories} />
+          </TwoColumnSidebar>
+        </TwoColumn>
+      </article>
+    </Container>
+  )
+}
+
+export async function getStaticPaths() {
+  const allSlugs = await getAllSlugs()
+
+  return {
+    paths: allSlugs.map(({slug}) => `/blog/${slug}`),
+    fallback: false,
+  }
+}
+
+export async function getStaticProps(context) {
+  const slug = context.params.slug
+  const post = await getPostBySlug(slug)
+  const description = extractText(post.content)
+  const eyecatch = post.eyecatch ?? eyecatchLocal
+
+  return {
+    props: {
+      title: post.title,
+      publish: post.publishDate,
+      content: post.content,
+      eyecatch: eyecatch,
+      categories: post.categories,
+      description: description,
+    }
+  }
+}
